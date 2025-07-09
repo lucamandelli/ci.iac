baef43e5eb4e14ce71b6cc29f5be7f51e38c7218
lucamandelli <luca.mandelli@poatek.com>
2025-07-09 17:32:06 -0300

    new: configure backend

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ terraform {
       version = "6.2.0"
     }
   }
+  backend "s3" {
+    bucket = "luca-iac"
+    key    = "state/terraform.tfstate"
+    region = "us-east-2"
+  }
 }
 
 provider "aws" {
