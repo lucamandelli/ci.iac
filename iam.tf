04b9c651556998ccbce914c314ed0a02981674d6
lucamandelli <luca.mandelli@poatek.com>
2025-07-09 16:43:01 -0300

    new: adding tf role and permissions

--- iam.tf
+++ iam.tf
@@ -9,6 +9,38 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
   }
 }
 
+resource "aws_iam_role" "tf-role" {
+  name = "tf-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::143320676585:oidc-provider/token.actions.githubusercontent.com"
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : [
+              "sts.amazonaws.com"
+            ]
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:lucamandelli/ci.iac:ref:refs/heads/main"
+            ]
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    IAC = "true"
+  }
+}
 resource "aws_iam_role" "ecr_role" {
   name = "ecr-role"
 
